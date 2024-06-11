2f102cfc30664e277d005752d802d0fa59428105
prok0l <egor.kuzovkin.zr@gmail.com>
2024-06-12 00:20:31 +0300

    add District model

--- nakormi_back/main/admin.py
+++ nakormi_back/main/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import District
+
+
 # Register your models here.
+
+@admin.register(District)
+class DistrictAdmin(admin.ModelAdmin):
+    list_display = ('name', 'city')
--- nakormi_back/main/models.py
+++ nakormi_back/main/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+
+class District(models.Model):
+    name = models.CharField(max_length=100)
+    city = models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.name
